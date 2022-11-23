10cf475d6ba6540b1d37f83eb9dbbc204eb4f346
Yerzin Perez <yerzin24@hotmail.com>
2022-11-22 19:46:19 -0500

    code

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,5 +3,4 @@ export namespace config {
   export const baseURL = 'http://127.0.0.1:5000/';
   export const cadenaBD = '';
   export const tipoComunicacion = 'email';
-  export const constvariable = 0;
 }
